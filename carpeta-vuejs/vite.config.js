f75387f97d23d3991df4ec8991af5a4ff362e0ac
Francisco Arenas <farenasp@users.noreply.github.com>
2026-02-17 17:21:21 -0300

    chore: update Vue.js project dependencies and configuration
    - Removed outdated Tailwind CSS PostCSS plugin from devDependencies in package.json.
    - Updated autoprefixer and postcss versions in package.json.
    - Changed tailwindcss version to 3.4.3 in package.json.
    - Deleted postcss.config.js as it was no longer needed.
    - Modified vite.config.js to set server host to true and changed open option to false; added polling for file watching.
    - Added vue_frontend service to docker-compose.yml for Vue.js application.
    - Created Dockerfile for building and running the Vue.js application with Vite.
    - Added postcss.config.cjs for configuring PostCSS with Tailwind CSS and autoprefixer.
    - Initialized package-lock.json for dependency management.

--- carpeta-vuejs/vite.config.js
+++ carpeta-vuejs/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true,
     port: 5173,
-    open: true
+    open: false,
+    watch: {
+      usePolling: true
+    }
   }
 })
